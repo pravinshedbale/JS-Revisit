9b7ca4b87560f9fb252bc714cd33a8415e5619f6
Pravin Shedbale <pravin.shedbale@outlook.com>
2023-03-27 16:41:02 +0530

    data types

---
+++ 1. Fundamentals/dataTypes.js
@@ -0,0 +1,28 @@
+// string
+let firstName = "Pravin";
+console.log(firstName, typeof firstName);
+
+// numbers
+let currentAge = 24;
+console.log(currentAge, typeof currentAge);
+
+// boolean
+let booleanTrue = true;
+let booleanFalse = false;
+console.log(booleanFalse, booleanTrue, typeof booleanTrue);
+
+// undefined
+let varUndefined;
+console.log(varUndefined, typeof varUndefined);
+
+// null
+let varNull = null;
+console.log(varNull, typeof varNull);
+
+// symbol
+let symbol = Symbol("INR");
+console.log(symbol, typeof symbol);
+
+// bigint
+let bigInt = 8n;
+console.log(bigInt, typeof bigInt);
